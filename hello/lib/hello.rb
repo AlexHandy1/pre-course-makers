cab73ed40e0ce90e494cc4b77b2305564d501e19
Alex Handy <handy437@gmail.com>
2015-04-03 08:59:56 +0100

    Updated Rakefile and Hello ex

--- hello/lib/hello.rb
+++ hello/lib/hello.rb
@@ -1,5 +1,7 @@
-#Default is "World"
-#Autor: Alex Handy (handy437@gmail.com)
+require 'greeter'
+
+# Default is World
 name = ARGV.first || "World"
 
-puts "Hello, #{name}!"
+greeter = Greeter.new(name)
+puts greeter.greet
