c5a1bedff5260b43efe50007a5b8e6a06211efd2
Alex Handy <handy437@gmail.com>
2015-04-19 17:34:13 +0100

    Array_Hash_String drills

--- Wk3Ruby/LRHW/ex45.rb
+++ Wk3Ruby/LRHW/ex45.rb
@@ -15,98 +15,142 @@
 	# - How do you build variables within classes that can be accessed/ compared directly within an inherited class (e.g. Opponent vs Player)
 	# - What does super do?
 
+#STEPS
+	#Review code for change requirements (DONE)
+	#Restructure classes - Level 1, Level 2, Level 3 and split out existing code (DONE)
+	#Rebuild initialisation to have a levels map and level engine (DONE)
+	#Tweak return values in each Level (DONE)
+	#Check everything runs through (DONE)
+	#Learn how to fix variables issue >> when try and carry across classes it is not working
+	#Review Opponent class - add module, try and get functionality working
+	#Try and get interactive functionality going for building on player
+	#Build test? >> set-up Rakefile in folder, see if can get working
 
 ### MAIN PROGRAMME ###
 
-class Player
-
-	def initialize
-			puts "Welcome to SMASH TENNIS DEMO"
-			puts "What's your name?"
-			puts ">"
-			user_name = $stdin.gets.chomp
-			@user_name = user_name
-
-			puts "thanks #{@user_name}, we are just creating your player..."
-
-			#research whether useful to use an object or hash here
-
-			@strength = 2
-			@speed = 2
-			@skill = 4
-			@confidence = 1
-
-			puts "Your player #{@user_name} has been created and he has #{@strength} for strength, #{@speed} for speed, #{@skill} for skill and #{@confidence} for confidence"
-			start_console
+class Level_Engine
+	def initialize(level_map)
+		@level_map = level_map
 	end
 
-			attr_accessor :strength #lets me access individual variable through an instance
-			attr_accessor :speed
-			attr_accessor :skill
-			attr_accessor :confidence
+	 def play() #need to update all the variables
+	    current_level = @level_map.opening_level()
+	    last_level = @level_map.next_level('finished') #need to add to all levels
+
+    while current_level != last_level
+      next_level_name = current_level.enter() #this stores the return output value of enter method in each class (e.g. next scene name)
+      current_level = @level_map.next_level(next_level_name) #this then initiated the next scene by using that return value in next scene method
+	end
+
+    # be sure to print out the last scene
+    	current_level.enter() #need to add to all the new classes
+  	end
+end	
+
+  class Start_Level
+
+  	def enter
+		puts "Welcome to SMASH TENNIS DEMO"
+		puts "What's your name?"
+		puts ">"
+		user_name = $stdin.gets.chomp
+		@user_name = user_name
+
+		puts "thanks #{@user_name}, we are just creating your player..."
+
+		@strength = 2
+		@speed = 2
+		@skill = 4
+		@confidence = 1
+
+		puts "Your player #{@user_name} has been created and he has #{@strength} for strength, #{@speed} for speed, #{@skill} for skill and #{@confidence} for confidence"
+		start_console
+	end 
 
 	def start_console
-			puts "Now your player has been created, you have some options"
+		puts "Now your player has been created, you have some options"
 
-			puts "There are 3 levels in the game 1: Pre-training, 2: Practice Session and 3: Tournament"
-			puts "The aim is to get your player to improve their attributes so they can get to the end of the Tournament and win!"
-			puts "Given this is a demo, you can choose to start at any level"
-			puts "Do you understand?"
-			answer = $stdin.gets.chomp
-			understand = false
+		puts "There are 3 levels in the game 1: Pre-training, 2: Practice Session and 3: Tournament"
+		puts "The aim is to get your player to improve their attributes so they can get to the end of the Tournament and win!"
+		puts "Given this is a demo, you can choose to start at any level"
+		puts "Do you understand?"
+		answer = $stdin.gets.chomp
+		understand = false
 
-			while understand == false
-				if answer == "YES"
-					understand = true
-				else
-					puts "Please re-read the instructions on your screen and when you understand answer 'YES'"
-					answer = $stdin.gets.chomp
-				end
+		while understand == false
+			if answer == "YES"
+				understand = true
+			else
+				puts "Please re-read the instructions on your screen and when you understand answer 'YES'"
+				answer = $stdin.gets.chomp
 			end
+		end
 
-			#initially tried to make this a CONSTANT (like in ex23) but said it was dynamic. Still works this way but worth revisiting
-			options = {
-				:'1' => :level1,
-				:'2' => :level2,
-				:'3' => :level3
-			}
+		#initially tried to make this a CONSTANT (like in ex23) but said it was dynamic. Still works this way but worth revisiting
+		level_map = {
+			:'1' => :level1,
+			:'2' => :level2,
+			:'3' => :level3
+		}
 
-			choice = nil
+		choice = nil
 
-			until options.keys.include?(choice)
-				puts "1: Pre-training"
-				puts "2: Practice Session"
-				puts "3: Tournament"
-				puts "Please select your level"
-				puts ">>"
+		until level_map.keys.include?(choice)
+			puts "1: Pre-training"
+			puts "2: Practice Session"
+			puts "3: Tournament"
+			puts "Please select your level"
+			puts ">>"
 
-				choice = gets.chomp!.to_s.upcase.to_sym
-			end
+			choice = gets.chomp!.to_s.upcase.to_sym
+		end
 
-			choice = options[choice]
+		choice = level_map[choice]
 
-			case choice
-			when :level1
-				level1_pre_training
-			when :level2
-				@strength += 2
-				@speed += 2
-				@skill += 2
-				@confidence +=2
-				level2_practice_session
-			when :level3
-				@strength += 5
-				@speed += 5
-				@skill += 5
-				@confidence += 5
-				level3_enter_tournament
-			end
+		case choice
+		when :level1
+			return 'level1'
+		when :level2
+			@strength += 2
+			@speed += 2
+			@skill += 2
+			@confidence +=2
+			return 'level2'
+		when :level3
+			@strength += 5
+			@speed += 5
+			@skill += 5
+			@confidence += 5
+			return 'level3'
+		end
 
+	end
+
+end
+
+class Level1
+
+	def enter
+		puts "Welcome to Level 1..."
+
+		puts "You get three goes to train your player for the next level"
+		puts "You can train your player in the gym, track or court by entering 'Gym', 'Track' or 'Court'"
+		
+		#NON INTERACTIVE VERSION#
+		prompt = "Hit Enter >>"
+
+		puts "First, your player needs to go the gym"
+		puts prompt
+		$stdin.gets.chomp
+
+		level1_gym
 	end
 
 	def level1_gym 
 		puts "Go to the gym..."
-		@strength += 2
+		#@strength += 2 not carrying through instance variables (threw an error)
+		puts "Awesome, now your player has #{@strength} for strength, #{@speed} for speed, #{@skill} for skill and #{@confidence} for confidence"
+		return 'level2'
 	end
 
 	def level1_track 
@@ -120,206 +164,86 @@ class Player
 		@confidence += 2
 	end
 
-	def level2_hit 
-			puts "Go have an intense hit on court"
-			@strength += 3
-			@speed += 3
-	end
 
+end
 
-	def level2_practice_match 
-			puts "Go have a full practice match - you're almost ready!"
-			@skill += 3
-			@confidence += 3
-	end
+class Level2
+	
+	def enter
+		puts "Welcome to Level 2..."
+		puts "So your player now has #{@strength} for strength, #{@speed} for speed, #{@skill} for skill and #{@confidence} for confidence"
+		puts "They still need to improve to enter a tournment though so lets get going"
 
-	def level1_pre_training
-			puts "Welcome to Level 1..."
+		#NON INTERACTIVE VERSION#
+		prompt = "Hit Enter >>"
 
-			puts "You get three goes to train your player for the next level"
-			puts "You can train your player in the gym, track or court by entering 'Gym', 'Track' or 'Court'"
-			
-			#NON INTERACTIVE VERSION#
-			prompt = "Hit Enter >>"
-
-			puts "First, your player needs to go the gym"
-			puts prompt
-			$stdin.gets.chomp
-			level1_gym
-			puts "Awesome, now your player has #{@strength} for strength, #{@speed} for speed, #{@skill} for skill and #{@confidence} for confidence"
-
-			puts "Now, your player needs some time on the track"
-			puts prompt
-			$stdin.gets.chomp
-			level1_track
-			puts "Awesome, now your player has #{@strength} for strength, #{@speed} for speed, #{@skill} for skill and #{@confidence} for confidence"
-
-			puts "Lastly, your player needs to get some practice on court"
-			puts prompt
-			$stdin.gets.chomp
-			level1_court
-			puts "Awesome, now your player has #{@strength} for strength, #{@speed} for speed, #{@skill} for skill and #{@confidence} for confidence"
-
-
-			#INTERACTIVE VERSION - WIP#
-
-			#issue about how get back into the while loop from function that call - get stuck in current structure, how approach?
-				# goes = 0
-				# goodAnswer = false
-				# level1_choice = nil
-
-				# while goes <= 3 && goodAnswer == false
-				# 	puts "Please enter Gym, Track or Court"
-				# 	puts ">>"
-				# 	level1_choice = $stdin.gets.chomp
-
-				# 	if level1_choice == "Gym"
-				# 		goes += 1
-				# 		level1_gym
-				# 		goodAnswer = true
-				# 	elsif level1_choice == "Track"
-				# 		goes += 1
-				# 		level1_track
-				# 		goodAnswer = true
-				# 	elsif level1_choice == "Court"
-				# 		goes += 1
-				# 		level1_court
-				# 		goodAnswer = true
-				# 	else
-				# 		puts "Please enter Gym, Track or Court"
-				# 		puts ">>"
-				# 		level1_choice = $stdin.gets.chomp
-				# 	end
-				# end
-						
-				if @skill >= 6 #add parameters to push up to the next level
-						puts "Well done, you can progress to the next level"
-						level2_practice_session
-				end
-
-	end
-
-	def level2_practice_session
-			puts "Welcome to Level 2..."
-			puts "So your player now has #{@strength} for strength, #{@speed} for speed, #{@skill} for skill and #{@confidence} for confidence"
-			puts "They still need to improve to enter a tournment though so lets get going"
-
-			#NON INTERACTIVE VERSION#
-			prompt = "Hit Enter >>"
-
-			puts "Now, your player needs to go for an intense hit"
-			puts prompt
-			$stdin.gets.chomp
-			level2_hit
-			puts "Awesome, now your player has #{@strength} for strength, #{@speed} for speed, #{@skill} for skill and #{@confidence} for confidence"
-
-			puts "Ok, before the tournament your player needs to have a full practice match"
-			puts prompt
-			$stdin.gets.chomp
-			level2_practice_match
-			puts "Awesome, now your player has #{@strength} for strength, #{@speed} for speed, #{@skill} for skill and #{@confidence} for confidence"
-
-
-				if @skill >= 9 #add parameters to push up to the next level
-							puts "Well done, you can progress to the next level"
-							level3_enter_tournament
-				end
-	end
-
-	def level3_enter_tournament
-			puts "Welcome to Level 3..."
-			puts "So your player now has #{@strength} for strength, #{@speed} for speed, #{@skill} for skill and #{@confidence} for confidence"
-			puts "You are now ready to enter your first tournament!"
-			puts "Do you want to enter the tournament?" #advanced feature would be to allow people to go back up and down levels? Add variable in parameters called acceptance that set true or false?
-
-			entry = $stdin.gets.chomp
-
-			if entry == "YES"
-					puts "OK good luck..."
-				if @confidence > 5 #PLACEHOLDER - update to >> certain attributes greater than opponent object that set-up then win else lose
-						puts "Congratulations, you win the first match in your tournament!"
-						puts "If you want to keep playing SMASH TENNIS, go to buysmashtennis.com!"
-				else 
-						puts "Ah back luck, you needed more confidence"
-						puts "If you want to keep playing SMASH TENNIS, go to buysmashtennis.com!"
-				end
-			else 
-				puts "Ok thanks for playing the SMASH TENNIS DEMO!"
-			end
-
-			# $player = TPlayer.new(
-			# 						:strength => @strength,
-			# 						:speed => @speed,
-			# 						:skill => @skill,
-			# 						:confidence => @confidence
-			# 					)
-			# puts $player
-			# return $player
+		puts "Now, your player needs to go for an intense hit"
+		puts prompt
+		$stdin.gets.chomp
+		return 'level3'
 	end
 
 end
 
-class Opponent < Player
-	# attr_accessor :name #option to add in name acceptance if get basics working
-	def initialize
-			@opp_strength = 2
-			@opp_speed = 2
-			@opp_skill = 4
-			@opp_confidence = 1
-	end
-			attr_accessor :opp_strength #lets me access individual variable through an instance
-			attr_accessor :opp_speed
-			attr_accessor :opp_skill
-			attr_accessor :opp_confidence
+class Level3
 
-	def test_gym 
-		puts "Go to the gym..."
-		@opp_strength += 2
+	def enter
+		puts "Welcome to Level 3..."
+		puts "So your player now has #{@strength} for strength, #{@speed} for speed, #{@skill} for skill and #{@confidence} for confidence"
+		puts "You are now ready to enter your first tournament!"
+		puts "Do you want to enter the tournament?" #advanced feature would be to allow people to go back up and down levels? Add variable in parameters called acceptance that set true or false?
+
+		entry = $stdin.gets.chomp
+		return 'opponent'
 	end
 
-	def test_track 
-		puts "Go to the track..."
-		@opp_speed += 2
-	end
-
-	def name_check
-		puts "Here is your opponent"
-		puts "Your opponent has has #{@opp_strength} for strength, #{@opp_speed} for speed, #{@opp_skill} for skill and #{@opp_confidence} for confidence"
-	end
-
-	$tester = Proc.new do |ignore|
-			puts "Here is where could put a proc solution"
-	end
-
-	def initiate_tournament(tester) #how do you access and compare a variable from one class in another class (e.g. Player attributes vs Opponent attributes)?
-		#adding a closure through proc or lambdas could be one solution
-		puts "You are going to play the first player"
-
-		tester.call
-		#practice a proc, lambda and/ or bloc here
-
-		# if user_player.strength > opponent.opp_strength
-		# 	puts "You win!"
-		# else
-		# 	puts "Sorry, you lose"
-		# end
-	end
-end 
-
-
-user_player = Player.new
-opponent = Opponent.new
-opponent.name_check
-opponent.test_gym
-opponent.test_track
-opponent.name_check
-puts opponent.opp_strength
-puts user_player.strength
-
-if user_player.strength > opponent.opp_strength # works outside of the class instances - need to understand why
-			puts "You win!"
-else
-			puts "Sorry, you lose"
 end
 
-opponent.initiate_tournament($tester)
+class Opponent
+	
+	def enter
+		puts "Here is your opponent..."
+		return 'finished'
+	end
+
+
+end
+
+class Finish
+	
+	def enter
+		puts "You reached the end"
+	end
+
+end
+
+class Level_Map
+	@@levels = {
+    'start_level' => Start_Level.new(),
+    'level1' => Level1.new(),
+    'level2' => Level2.new(),
+    'level3' => Level3.new(),
+    'opponent' => Opponent.new(),
+    'finished' => Finish.new(),
+  }
+
+
+  attr_accessor :strength, :speed, :skill, :confidence
+
+	def initialize(start_level)
+		@start_level = start_level
+	end
+
+	def next_level(level)
+	    val = @@levels[level]
+	    return val
+	end
+
+	def opening_level()
+	    return next_level(@start_level)
+	end
+end
+
+map = Level_Map.new('start_level')
+match = Level_Engine.new(map)
+match.play()
--- Wk3Ruby/practice/arrays_hashes.rb
+++ Wk3Ruby/practice/arrays_hashes.rb
@@ -1,1 +1,175 @@
 #DRILLS FOR COMMON ARRAY AND HASH PROBLEMS#
+
+#You are given an array and you use the array to add keys and values iteratively to a new hash which you then invert
+def method_1(array)
+	p array
+	easy_hash = Hash[*array] #need to remember the splat star when passing in
+	puts easy_hash
+
+	hash = Hash[array.map{|k| [k, "value"]}] #>> creates new key value pairs but not quite what looking for
+	puts hash
+
+	new_hash = {}
+	array.each_with_index do |k,v|
+		new_hash[k] = v
+	end 
+	puts new_hash.invert
+end
+
+#method_1(["This", "is", "an", "example", "array", "to a hash"])
+
+#You manually create a new hash and then sort this hash by its values and create an array from it
+
+def method_2
+	hash = Hash.new(0)
+
+	hash['one'] = 2
+	hash['two'] = 1
+	hash['three'] = 3
+	puts hash
+
+	sorted = hash.sort_by {|k,v| v} #returns an array 
+	puts sorted.is_a?(Array)
+end
+
+#method_2
+
+#You iterate over a hash's existing keys and values and create a third layer based on its index
+
+def method_3
+	hash_newb = {
+		:test1 => :'testing1',
+		:test2 => :'testing2',
+		:test3 => :'testing3'
+	}
+
+		# hash_newb.inject({})
+
+end
+
+#method_3
+
+#You are given a string, that you turn into an array, that you iterate over to create a hash based on count of word
+
+def method_4(string)
+	array = string.scan(/\w+/)
+	hasher = Hash.new(0)
+	array.each do |key|
+		hasher[key] += 1
+	end
+	puts hasher
+end
+
+#method_4("This is a string right here here here thats going to become a hash hash hash")
+
+#You make method to compare two arrays, flatten, compare and remove duplicates, then create a hash based on index
+
+def method5(array1, array2)
+	new_array = array1 | array2
+	new_array2 = array1 & array2
+	new_array3 = array1 - array2
+	# puts new_array
+	# puts new_array2
+	#puts new_array3
+
+	one_array = array1.zip(array2).flatten.uniq #merges side by side with zip and then .flatten puts into one array
+
+	non_duplicate_hash = {}
+	one_array.each_with_index do |k,v|
+		non_duplicate_hash[k] = v
+	end
+	puts non_duplicate_hash
+end
+#method5 ["This", "is", "an", "example", "array", "one"], ["This", "is", "an", "example", "array", "two"]
+
+
+#You are given a string and you iterate over it to provide an array of incremented characters e.g. given abcd >> ['b', 'c', 'd', 'e']
+
+def method6(str)
+	result = []
+	str.each_char do |x| 
+		result << (x.ord+1).chr
+	end
+	puts result
+end
+#method6("Herewego")
+
+#You are given a two sentences, one as a string and another as an array and you replace punctuation with .scan and .gsub and then merge
+
+def method7(str, array)
+	#must be a quicker way but...
+
+	#remove punctuation
+	clean_string = str.gsub(/[[:punct:]]/, "")
+	array.map! {|element| element.gsub(/[[:punct:]]/, "")}
+	
+	#combine
+	clean_array = clean_string.scan(/\w+/).zip(array).flatten
+	#remove whitespace
+	final_array = clean_array.compact.delete_if{ |x| x == ''}
+	puts final_array
+
+end
+#method7("Here are some strings with ? wrong !! stuff", ['H!E!R!E', 'W?E?', 'GO//'])
+
+
+#You are given an array which you need to search filter by every odd index number
+
+def method8(array)
+	results = []
+
+	array.length.times do |index|
+		results << array[index] if index.odd?
+	end
+	puts results
+	
+	now_even = []
+
+	results.each_with_index do |word, index|
+		now_even << word if index.even?
+	end
+	puts now_even
+end
+
+# method8(["Blah", "This", "Blah", "Will", "Blah", "Work", "Blah", "More", "Blah", "Than", "Blah", "Once"])
+
+
+#Injection example into a hash
+
+def method9(array)
+	hash = array.inject({}) {|a,i| a.update(i => i)}
+	puts hash
+
+	hash_within_hash = array.inject({}) {|a,i| a.update(i => a)}
+	puts hash_within_hash
+end
+
+#method9(['Key One', '1', 'Key Two', '2', 'Key Three', '3'])
+
+#Take a string of numbers, split into pairs and then select only those > 5
+
+def method10(array)
+	new_arrays = []
+	array.select!{|x| x > 5}.each_slice(2) {|a,b| new_arrays << [a,b]}
+	#p new_arrays
+	return new_arrays
+end
+
+#You are given a multiple dimension array and you need to add 2 to each element of the array
+def method11(arrays)
+
+	new_array = arrays.each do |subarray|
+		subarray.collect! {|a| a + 2}
+	end
+	p new_array
+
+end
+
+multiple_arrays = method10([2,3,3,2,4,5,6,7,10,15,17,21])
+p multiple_arrays
+
+method11(multiple_arrays)
+
+
+
+
